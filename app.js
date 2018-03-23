2bd5bc2eaa69df3de13b7c9f5a44f98222aa5155
dngoyb <dngoyb@gmail.com>
2018-03-23 16:32:02 +0200

    printing formatted address

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=41%20Juta%20street',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20juta%20street',
     json: true
 }, (err, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
 });
